a37448822de4e7d91e24e86ec4803fe0c3ae854b
SW2125\dltjd <leehj050211@gmail.com>
2021-11-20 19:39:06 +0900

    add: command git log, fix: 예외 처리

--- jjap-git.cpp
+++ jjap-git.cpp
@@ -9,40 +9,58 @@ typedef struct STACK{
 	STACK * next;
 }STACK;
 //function
-STACK* init(void);
+STACK* init();
 STACK* commit(STACK* node, char * data);
+void log(STACK* node);
 
 int main(){
 
-	STACK * node;
+	STACK * node = NULL;
 	
 	char data[100];
 	
 	//system
-	printf("짭 git에 오신 것을 환영합니다\n밑에서 git 명령어를 입력해주세요. exit를 입력해 프로그램 종료\n");
+	printf("짭 git에 오신 것을 환영합니다\ngit 명령어를 입력해주세요. exit를 입력해 프로그램 종료\n");
 	while(strcmp(command, "exit")!=0){
 		gets(command);
 		if(strcmp(command, "git init")==0){
 			node = init();
 		}
 		else if(strcmp(command, "git commit -m")==0){
-			printf("커밋 메시지를 입력해주세요");
-			gets(data); 
-			node = commit(node, data);
+			if(!node){
+				printf("먼저 git init을 해주세요.\n");
+			}else{
+				printf("커밋 메시지를 입력해주세요\n");
+				gets(data); 
+				node = commit(node, data);
+			}
+		}
+		else if(strcmp(command, "git log")==0){
+			if(!node){
+				printf("먼저 git init을 해주세요.\n");
+			}else{
+				log(node);
+			}
 		}
 	}
 	return 0;
 }
-STACK * init(void){
+STACK* init(){
 	STACK *node = (STACK*)malloc(sizeof(STACK));
 	node->next = NULL;
 	return node;
 }
-STACK * commit(STACK* node, char * data){
+STACK* commit(STACK* node, char * data){
 	STACK *temp = (STACK*)malloc(sizeof(STACK));
 	temp -> next = NULL;
 	strcpy( temp -> buf, data);
 	node -> next = temp;
-	return node; 
+	return node;
+}
+void log(STACK* node){
+	while(node){
+		printf("%s\n", node -> buf);
+		node=node -> next;
+	}
 }
 
